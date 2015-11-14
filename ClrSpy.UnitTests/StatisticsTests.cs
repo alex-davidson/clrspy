2be38227e3b7e96dfc84288ab0146b8ab21f1da9
Alex Davidson <alex.davidson@bluewire-technologies.com>
2015-11-14 15:08:49 +0000

    Add simple statistics class.
    Intended for future use with heap dump.

---
+++ ClrSpy.UnitTests/StatisticsTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClrSpy.UnitTests
+{
+    public class StatisticsTests
+    {
+        private static readonly List<ulong> set = new List<ulong> {
+            0, 3, 4, 1, 2,
+            7, 9, 3, 4, 5,
+            5, 5, 6, 3, 1,
+            1, 2, 9, 2, 9,
+            3, 4
+        };
+
+        [Test]
+        public void TestSetsAreEquivalent()
+        {
+            Assert.That(sortedSet, Is.EquivalentTo(set));
+        }
+
+        [Test]
+        public void StatisticsAreCorrect()
+        {
+            var stats = Statistics.CalculateInPlace(set.ToArray());
+
+            Assert.That(stats.Total, Is.EqualTo(88));
+            Assert.That(stats.Count, Is.EqualTo(22));
+            Assert.That(stats.Minimum, Is.EqualTo(0));
+            Assert.That(stats.Maximum, Is.EqualTo(9));
+            
+            Assert.That(stats.Mean, Is.EqualTo(4));
+            Assert.That(stats.Median, Is.EqualTo(3.5));
+
+            Assert.That(stats.Variance, Is.EqualTo(150d / 22));
+            Assert.That(stats.StandardDeviation, Is.EqualTo(Math.Sqrt(150d / 22)));
+        }
+
+        [Test]
+        public void CalculatesMedianOfEvenSizedSet()
+        {
+            var stats = Statistics.CalculateInPlace(new ulong[] { 1, 2, 4, 8 });
+            Assert.That(stats.Median, Is.EqualTo(3));
+        }
+        
+        [Test]
+        public void CalculatesMedianOfOddSizedSet()
+        {
+            var stats = Statistics.CalculateInPlace(new ulong[] { 1, 2, 4 });
+            Assert.That(stats.Median, Is.EqualTo(2));
+        }
+
+        // Sorted version of the test set, for easier manual verification of results:
+        private readonly List<ulong> sortedSet = new List<ulong> {
+            0,
+            1, 1, 1,
+            2, 2, 2,
+            3, 3, 3, 3,
+            4, 4, 4,
+            5, 5, 5,
+            6,
+            7,
+            9, 9, 9
+        };
+        /*
+            Sum : 88
+            Count: 22
+            Mean: 4
+            Sum of squared differences from the mean: 150
+        */
+
+    }
+}
---
+++ ClrSpy/Statistics.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClrSpy
+{
+    public class Statistics
+    {   
+        // NOTE: Modifies the input array.
+        public static Statistics CalculateInPlace(ulong[] values)
+        {
+            if(values.LongLength == 0) throw new ArgumentException("Empty array", nameof(values));
+            Array.Sort(values);
+
+            var stats = new Statistics {
+                Count = (ulong)values.LongLength,
+                Minimum = values.First(),
+                Maximum = values.Last(),
+                Median = GetMedian(values)
+            };
+
+            for(long i = 0; i < values.LongLength; i++)
+            {
+                stats.Total += values[i];
+            }
+            stats.Mean = (double)stats.Total / stats.Count;
+            stats.Variance = GetVariance(values, stats.Mean);
+            stats.StandardDeviation = Math.Sqrt(stats.Variance);
+
+            return stats;
+        }
+
+        private static double GetVariance(ulong[] values, double mean)
+        {
+            double sum = 0;
+            double sumOfSquares = 0;
+            for(long i = 0; i < values.LongLength; i++)
+            {
+                var d = values[i] - mean;
+                sum += d;
+                sumOfSquares += d*d;
+            }
+            var squareOfSums = sum * sum; 
+            // Note: this is the population variance, not the sample variance.
+            return (sumOfSquares - (squareOfSums / values.LongLength)) / values.LongLength;
+        }
+
+        private static double GetMedian(ulong[] array)
+        {
+            if(array.LongLength % 2 == 1)
+            {
+                var pos = (array.LongLength - 1) / 2;
+                return array[pos];
+            }
+            else
+            {
+                var pos = array.LongLength / 2;
+                return (double)(array[pos] + array[pos - 1]) / 2;
+            }
+        }
+
+        public ulong Count { get; set; }
+        public ulong Total { get; set; }
+        public double Mean { get; set; }
+        public double Median { get; set; }
+        /// <summary>
+        /// Population variance.
+        /// </summary>
+        public double Variance { get; set; }
+        public double StandardDeviation { get; set; }
+        public ulong Maximum { get; set; }
+        public ulong Minimum { get; set; }
+    }
+}
