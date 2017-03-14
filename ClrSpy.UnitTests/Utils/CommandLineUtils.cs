cb588f5d52eec7060a482a0b4f621d7ad1c88ed0
Alex Davidson <alex.davidson@bluewire-technologies.com>
2017-03-14 18:47:17 +0000

    Add tools for testing against processes

---
+++ ClrSpy.UnitTests/Utils/CommandLineUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ClrSpy.UnitTests.Utils
+{
+    public static class CommandLineUtils
+    {
+        private static readonly Regex rxSimpleArgument = new Regex(@"^[-\w\d/\\:\.]+$", RegexOptions.Compiled);
+
+        public static string QuoteArgument(string arg)
+        {
+            if (rxSimpleArgument.IsMatch(arg)) return arg;
+            return $"\"{arg.Replace("\"", "\"\"")}\"";
+        }
+    }
+}
