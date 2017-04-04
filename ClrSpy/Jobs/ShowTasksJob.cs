7c1f69fd6cc5e4aa37ce293f2b458f7babb1d912
Alex Davidson <alex.davidson@bluewire-technologies.com>
2017-04-04 19:53:34 +0100

    indented output

--- ClrSpy/Jobs/ShowTasksJob.cs
+++ ClrSpy/Jobs/ShowTasksJob.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using ClrSpy.CliSupport;
@@ -27,29 +29,96 @@ namespace ClrSpy.Jobs
                 var graph = new ClrHeapTaskAnalyser(runtime).BuildTaskGraph();
                 foreach (var task in graph.TaskVertices)
                 {
-                    var waiting = graph.Outgoing(task).ToArray();
-                    output.WriteLine($"{DescribeTask(task)} ({waiting.Length} continuations)");
-                    foreach (var continuation in waiting)
-                    {
-                        output.WriteLine($"    {DescribeContinuation(graph, continuation)}");
-                    }
+                    if (graph.CountIncoming(task) > 0) continue;
+                    WalkTask(new IndentedTextWriter(output), graph, task, 0);
                     output.WriteLine();
                 }
             }
-            console.WriteLine("Not implemented.");
         }
-        
-        private string DescribeObject(IClrCompositeObject obj)
+
+        class IndentedTextWriter
+        {
+            private int depth = 0;
+            private readonly TextWriter inner;
+
+            public IndentedTextWriter(TextWriter inner)
+            {
+                this.inner = inner;
+            }
+
+            public IndentScope Indent()
+            {
+                return new IndentScope(this);
+            }
+
+            public void WriteLine(string line)
+            {
+                inner.Write(new String(' ', depth * 4));
+                inner.WriteLine(line);
+            }
+
+            public struct IndentScope : IDisposable
+            {
+                private readonly IndentedTextWriter writer;
+
+                public IndentScope(IndentedTextWriter writer)
+                {
+                    this.writer = writer;
+                    writer.depth++;
+                }
+
+                public void Dispose()
+                {
+                    writer.depth--;
+                }
+            }
+        }
+
+        private void WalkTask(IndentedTextWriter output, TaskGraph graph, ClrClassObject task, int waitedOnCount)
+        {
+            if (waitedOnCount > 1)
+            {
+                output.WriteLine($"{DescribeTask(task)} ({waitedOnCount} antecedents)");
+            }
+            else
+            {
+                output.WriteLine(DescribeTask(task));
+            }
+            using (output.Indent())
+            {
+                var waiting = graph.Outgoing(task).ToArray();
+                WalkContinuations(output, graph, waiting);
+            }
+        }
+
+        private void WalkContinuations(IndentedTextWriter output, TaskGraph graph, IClrCompositeObject[] waiting)
+        {
+            foreach (var continuation in waiting)
+            {
+                var maybeTask = continuation as ClrClassObject;
+                if (maybeTask != null && maybeTask.IsOfTaskType())
+                {
+                    var waitedOnCount = graph.CountIncoming(maybeTask);
+                    WalkTask(output, graph, maybeTask, waitedOnCount);
+                }
+                else
+                {
+                    output.WriteLine(DescribeContinuation(graph, continuation));
+                }
+            }
+        }
+
+        private static string DescribeObject(IClrCompositeObject obj)
         {
             return $"{obj.Type.Name} @ {obj.Address:X16}";
         }
 
-        private string DescribeTask(ClrClassObject task)
+        private static string DescribeTask(ClrClassObject task)
         {
             return DescribeObject(task);
         }
 
-        private string DescribeContinuation(TaskGraph graph, IClrCompositeObject continuation)
+        private static string DescribeContinuation(TaskGraph graph, IClrCompositeObject continuation)
         {
             var others = graph.CountIncoming(continuation) - 1;
             if (others > 0)
