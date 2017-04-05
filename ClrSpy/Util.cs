496e61e3c38d4587bfee7e5c1eb881aaf8c2141b
Alex Davidson <alex.davidson@bluewire-technologies.com>
2017-04-05 19:16:05 +0100

    Provide a 'safe' forced cast for use with generics
    When an unconstrained generic type T has been tested at runtime to be of
    a specific type, we must often cast via 'object' to satisfy the
    compiler. But this causes the source type T to be ignored entirely, and
    refactoring the surrounding code may cause the type of the source
    variable to change, potentially invalidating our assumptions about the
    validity of the cast.
    
    To avoid accidental 'silent' changes to the source type, the ForceCast<,>
    method requires that both the source and destination types be specified
    explicitly.

--- ClrSpy/Util.cs
+++ ClrSpy/Util.cs
@@ -23,5 +23,18 @@ namespace ClrSpy
             list.RemoveAt(lastIndex);
             return item;
         }
+
+        /// <summary>
+        /// Cast via 'object' to escape compiler type checking, but do so 'safely': this method requires explicit
+        /// specification of both input and output types to try to defeat accidental 'clever' fixups by refactoring tools.
+        /// </summary>
+        /// <typeparam name="TInput"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static TOutput ForceCast<TInput, TOutput>(TInput input)
+        {
+            return (TOutput)(object)input;
+        }
     }
 }
