c4a33448bf45f17833195958335cf5c77fc47990
Alex Davidson <alex.davidson@bluewire-technologies.com>
2017-09-16 13:56:55 -0700

    Implement simple tempfile helper

---
+++ ClrSpy.UnitTests/Utils/TemporaryFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClrSpy.UnitTests.Utils
+{
+    public class TemporaryFile : IDisposable
+    {
+        public string Path { get; }
+
+        public TemporaryFile()
+        {
+            Path = System.IO.Path.GetTempFileName();
+        }
+
+        public void Delete()
+        {
+            if (File.Exists(Path)) File.Delete(Path);
+        }
+
+        public void Dispose()
+        {
+            Delete();
+        }
+    }
+}
